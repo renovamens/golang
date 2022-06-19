5bf056b36c5113c1b1aba36c34c7d73da497359b
Jeremy-boo <bozhu@alauda.io>
2022-06-19 13:55:31 +0800

    add unit test

---
+++ test/add.go
@@ -0,0 +1,9 @@
+package test
+
+func add(a, b int) int {
+	return a + b
+}
+
+func Increase(a, b int) {
+	add(a, b)
+}
---
+++ test/add_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestIncrease(t *testing.T) {
+	t.Log("start testing")
+	Increase(1, 2)
+}
